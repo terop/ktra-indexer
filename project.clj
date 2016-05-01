2709193dd555f052dd03418f7d6deb40cf1f5dc1
Tero Paloheimo <tero.paloheimo@iki.fi>
2016-05-01 18:51:25 +0300

    Update Leiningen dependencies

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
 (defproject ktra-indexer "0.1.0-SNAPSHOT"
   :description "A simple application for indexing and searching KTRA track
   listings"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/terop/ktra-indexer"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.0"]
+                 [ring/ring-defaults "0.2.0"]
                  [ring/ring-devel "1.4.0"]
-                 [org.immutant/web "2.1.2"]
-                 [selmer "1.0.0"]
-                 [cheshire "5.5.0"]
+                 [org.immutant/web "2.1.4"]
+                 [selmer "1.0.4"]
+                 [cheshire "5.6.1"]
                  [org.postgresql/postgresql "9.4.1207"]
                  [korma "0.4.2"]
-                 [org.clojure/java.jdbc "0.4.2"]
+                 [org.clojure/java.jdbc "0.5.8"]
                  [clj-time "0.11.0"]
-                 [buddy/buddy-auth "0.9.0"]
+                 [buddy/buddy-auth "0.13.0"]
                  [com.yubico/yubico-validation-client2 "3.0.1"]
                  [org.apache.commons/commons-lang3 "3.4"]]
   :main ktra-indexer.handler
