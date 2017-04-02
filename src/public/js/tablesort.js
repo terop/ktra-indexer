83c0470e0b1701edd34caa19dbe83270a45199ea
Tero Paloheimo <tero.paloheimo@iki.fi>
2017-04-02 17:42:08 +0300

    Add jQuery tablesorter to all tables

---
+++ src/public/js/tablesort.js
@@ -0,0 +1,26 @@
+
+var addTablesorter = function (tableId) {
+    // Add a hack to sort Finnish style dates
+    $.tablesorter.addParser({
+        id: 'fidate',
+        is: function(s) {
+            return false;
+        },
+        format: function(s, table) {
+            s = s.replace(/\-/g, '.');
+            s = s.replace(/(\d{1,2})\.(\d{1,2})\.(\d{4})/, '$3/$2/$1');
+            return $.tablesorter.formatFloat(new Date(s).getTime());
+        },
+        type: 'numeric'
+    });
+
+    $(tableId).tablesorter({
+        theme: 'bootstrap',
+        widgets: ['uitheme'],
+        usNumberFormat: 'false',
+        dateFormat: 'dd.mm.yyyy',
+        headers: {
+            2: { sorter: 'fidate' }
+        }
+    });
+};
