8b94881ac12c77b84a4bc71d04807a55513f88a6
Tero Paloheimo <tero.paloheimo@iki.fi>
2018-01-30 20:17:29 +0200

    Add a script for cleaning up artists in the database
    The script uses edit distance to find similar artists.

---
+++ db_cleaner/db_cleaner.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+# This a script for cleaning up the ktra-indexer database from similar
+# artists names.
+
+import argparse
+import sys
+from collections import OrderedDict
+
+import editdistance
+import psycopg2
+
+
+class DbCleaner(object):
+    """A class for performing the actual cleaning."""
+
+    def __init__(self, db_name, db_user, db_password):
+        """Class constructor."""
+        self._conn = psycopg2.connect(dbname=db_name, user=db_user,
+                                      password=db_password)
+        self._cursor = self._conn.cursor()
+        self._artists = OrderedDict()
+
+    def get_artists(self):
+        """Get all artists from the database."""
+        self._cursor.execute('SELECT DISTINCT ON (name) name, artist_id '
+                             'FROM artists ORDER BY name')
+        artists = self._cursor.fetchall()
+        for artist in artists:
+            self._artists[artist[0]] = artist[1]
+
+    def check_artists(self, edit_distance):
+        """Goes through each artists, filters them by edit distance and asks the user
+        whether to merge to a another one."""
+        self.get_artists()
+
+        print('Press \'q\' to quit')
+        print('Using an edit distance of {}'.format(edit_distance))
+
+        while self._artists:
+            keys = list(self._artists.keys())
+
+            distances = [[art, editdistance.eval(keys[0], art)] for art in keys]
+            filtered = list(filter(lambda item: item[1] <= edit_distance, distances))
+
+            if len(filtered) == 1:
+                self._artists.pop(filtered[0][0])
+                continue
+
+            print('Artists similar to "{}":'.format(filtered[0][0]))
+            print('0. Retain all')
+            for index, artist in enumerate(filtered):
+                print('{}. {} (edit distance {})'.format(index + 1, artist[0], artist[1]))
+
+            choice = input('Choose option: ')
+            choice = choice.rstrip('.')
+            if choice == 'q':
+                print('Stopping')
+                break
+            choice = int(choice)
+            if choice > len(filtered):
+                choice = input('Please choose an option between 0 and {}: '.format(len(filtered)))
+                choice = int(choice.rstrip('.'))
+
+            if choice == 0:
+                for index, artist in enumerate(filtered):
+                    self._artists.pop(filtered[index][0])
+                continue
+
+            new_artist_id = self._artists[filtered[choice - 1][0]]
+
+            for index, artist in enumerate(filtered):
+                if index == (choice - 1):
+                    self._artists.pop(filtered[index][0])
+                    continue
+
+                try:
+                    old_id = self._artists[filtered[index][0]]
+                    self._cursor.execute('UPDATE tracks SET artist_id = %s WHERE artist_id = %s',
+                                         (new_artist_id, old_id))
+                    self._cursor.execute('DELETE FROM artists WHERE artist_id = %s', (old_id,))
+                except psycopg2.DatabaseError as dbe:
+                    self._cursor.rollback()
+                    print('Error: got a database error: {}'.format(dbe.pgerror), file=sys.stderr)
+                    return False
+
+                self._artists.pop(filtered[index][0])
+
+        self._conn.commit()
+        return True
+
+
+def main():
+    """Module main function."""
+    parser = argparse.ArgumentParser(description='Cleans up a ktra-indexer database by removing'
+                                     ' artists whose names are close to each other. The "closeness"'
+                                     ' is compared with edit distance; an edit distance over 2'
+                                     ' is practically unusable.')
+    parser.add_argument('db_name', type=str, help='Name of the database')
+    parser.add_argument('db_user', type=str, help='Database user name')
+    parser.add_argument('db_password', type=str, help='Database user password')
+    parser.add_argument('--distance', type=int, help='Default edit distance, now 1', default=1)
+
+    args = parser.parse_args()
+
+    cleaner = DbCleaner(args.db_name, args.db_user, args.db_password)
+    cleaner.check_artists(args.distance)
+
+
+if __name__ == '__main__':
+    main()
