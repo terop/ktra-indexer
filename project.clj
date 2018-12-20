4593f447ec8cde6bda3ab70da7bdba029da6995d
Tero Paloheimo <tero.paloheimo@iki.fi>
2018-12-20 07:21:38 +0200

    Update dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   listings"
   :url "https://github.com/terop/ktra-indexer"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
                  [ring/ring-devel "1.7.1"]
