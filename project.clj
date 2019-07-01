75ffb5aa86589389072499622b4c88e52e36de76
Tero Paloheimo <tero.paloheimo@iki.fi>
2019-07-01 19:32:29 +0300

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,18 +3,18 @@
   listings"
   :url "https://github.com/terop/ktra-indexer"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
                  [ring/ring-devel "1.7.1"]
                  [org.immutant/web "2.1.10"]
-                 [selmer "1.12.8"]
+                 [selmer "1.12.12"]
                  [cheshire "5.8.1"]
-                 [org.postgresql/postgresql "42.2.5"]
+                 [org.postgresql/postgresql "42.2.6"]
                  [org.clojure/java.jdbc "0.7.9"]
                  [honeysql "0.9.4"]
                  [clj-time "0.15.1"]
-                 [buddy/buddy-auth "2.1.0"]
+                 [buddy/buddy-auth "2.2.0"]
                  [com.yubico/yubico-validation-client2 "3.0.5"]
                  [org.apache.commons/commons-text "1.6"]
                  [org.clojure/tools.logging "0.4.1"]
