a73728b5362b5e9649d1cb8a008f7b08e2ce418f
Tero Paloheimo <tero.paloheimo@iki.fi>
2019-11-18 21:52:42 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
-                 [ring/ring-devel "1.7.1"]
+                 [ring/ring-devel "1.8.0"]
                  [org.immutant/web "2.1.10"]
                  [selmer "1.12.17"]
                  [cheshire "5.9.0"]
@@ -18,7 +18,7 @@
                  [com.yubico/yubico-validation-client2 "3.0.5"]
                  [org.apache.commons/commons-text "1.8"]
                  [org.clojure/tools.logging "0.5.0"]
-                 [org.slf4j/slf4j-log4j12 "1.7.28"]]
+                 [org.slf4j/slf4j-log4j12 "1.7.29"]]
   :main ktra-indexer.handler
   :aot [ktra-indexer.handler
         clojure.tools.logging.impl]
